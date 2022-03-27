40f799318398fde07e3736d460b0bde1ccfb3e4b
Aiden Lin <98539623+AidenL173@users.noreply.github.com>
2022-03-27 23:47:51 +0000

    Part A

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-
 def seq3np1(n):
-    """
-        Print the 3n+1 sequence from n, terminating when it reaches 1.
-        args: n (int) starting value for 3n+1 sequence
-        return: None
-    """
-    while(n != 1):
-        print(n)
-        if(n % 2) == 0:        # n is even
-            n = n // 2
-        else:                 # n is odd
-            n = n * 3 + 1
-    print(n)                  # the last print is 1
+  count = 0
+  while(n != 1):
+    count = count + 1
+    if(n % 2) == 0:        # n is even
+      n = n // 2
+    else:                 # n is odd
+      n = n * 3 + 1
+  return(count)
 
 def main():
-	seq3np1(3)
+  start = int(input("Upper Bound? "))
+  if (start <= 0):
+    exit()
+  for i in range(1,(start + 1)):   
+    iterations = seq3np1(i)
+    print("N value is", i, "and number of iterations is",  iterations)
 main()
