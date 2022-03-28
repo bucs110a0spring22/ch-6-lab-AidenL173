4268160a45747144648676ef980f2203ba774be7
Aiden Lin <98539623+AidenL173@users.noreply.github.com>
2022-03-28 02:40:02 +0000

    Part B (No Clue)

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ Grace days remaining: < # >/5
  < What parts of the project you were not able to complete >
 
 ## REFERENCES:
- < List any outside resources used >
+https://docs.python.org/3.7/library/turtle.html#turtle.setworldcoordinates
 
 ## MISCELLANEOUS COMMENTS:
  < Anything you would like the grader to know >
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import turtle
+
 def seq3np1(n):
   count = 0
   while(n != 1):
@@ -16,3 +18,16 @@ def main():
     iterations = seq3np1(i)
     print("N value is", i, "and number of iterations is",  iterations)
 main()
+
+def graphData(mywindow=None, myturtle=None, writer=None, upperbound=None):
+  if (upperbound <= 0):
+    exit()
+  mywindow = turtle.Screen()
+  myturtle = turtle.Turtle()
+  writer = turtle.Turtle()
+  mywindow.setworldcoordinates(0, 0, 10, 10)
+  max_so_far = 0
+  for i in range(1,(upperbound + 1)):   
+    result = seq3np1(i)
+    if (result > max_so_far):
+      max_so_far = result 
